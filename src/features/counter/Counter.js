850e02269c6e814a5310d48db45460b3d32dc78e
Chi Fung Chan <cf.chan0318@gmail.com>
2022-06-23 16:55:03 +0800

    Redux set 1

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,18 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux';
-import {increment, decrement} from './counterSlice'
+import { increment, decrement, reset, incrementByAmount } from './counterSlice'
 
 const Counter = () => {
-    const count = useSelector((state) => state.counter.count);
-    const dispatch = useDispatch();
+  const count = useSelector((state) => state.counter.count);
+  const dispatch = useDispatch();
+
+  const [incrementAmt, setIncrementAmt] = useState(0);
+  const addValue = Number(incrementAmt) || 0;
+
+  const resetAll = () => {
+    setIncrementAmt(0);
+    dispatch(reset());
+  }
 
   return (
-      <section>
-          <p>{count}</p>
-          <div>
-              <button onClick={()=>{dispatch(increment())}}>+</button>
-              <button onClick={()=>{dispatch(decrement())}}>-</button>
-          </div>
+    <section>
+      <p>{count}</p>
+      <div>
+        <button onClick={() => { dispatch(increment()) }}>+</button>
+        <button onClick={() => { dispatch(decrement()) }}>-</button>
+      </div>
+
+      <input type="text" value={incrementAmt} onChange={(e) => { setIncrementAmt(e.target.value) }}></input>
+      
+      <div>
+        <button onClick={() => { dispatch(incrementByAmount(addValue)) }}>add amount</button>
+        <button onClick={()=>{dispatch(reset())}}>Reset</button>
+      </div>
     </section>
   )
 }
